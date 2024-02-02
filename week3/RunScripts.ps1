615835a4e0ade0d33afa98efa36ae61209e34931
unknown <isaiah.river@mymail.champlain.edu>
2024-02-02 12:39:27 -0500

    commit for week3

---
+++ week3/RunScripts.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot Deliverable-01.ps1)
+
+clear
+
+# Get Login and logouts from the last 15 days
+$winlogonTable = GetLogonEvents 15
+$winlogonTable
+# Get shutdowns from the last 15 days
+$PowerTable = StartAndShutdown 15
+$PowerTable 
+# Get Start ups from the last 15 days
