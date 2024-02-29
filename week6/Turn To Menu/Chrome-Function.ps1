83e309d6670f227592a840f15d67c6d5876e7e75
unknown <isaiah.river@mymail.champlain.edu>
2024-02-29 12:11:24 -0500

    Commit for Week6

---
+++ week6/Turn To Menu/Chrome-Function.ps1
@@ -0,0 +1,14 @@
+﻿function Chrome(){
+    $chromeProcess = Get-Process -Name *chrome*
+# Check if chrome is not running,
+    if ($chromeProcess -eq $null) {
+    # if it isn't, start it and navigate to champlain.edu
+    Start-Process "chrome" -ArgumentList "https://www.champlain.edu"
+    Write-Host "There was no instance of Chrome running, starting Chrome"
+    } 
+    else {
+    # If Chrome is running, close chrome
+    Stop-Process -Name *chrome*
+    Write-Host "Closing Chrome, as it was running."
+    }
+}
